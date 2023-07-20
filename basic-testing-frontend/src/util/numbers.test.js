3c3ef46b35319aa4d3d5046f6553a0cc55e545ab
Ali Nauman <alinauman97@outlook.com>
2023-07-21 01:27:56 +0500

    Improve the tests for transformToNumber

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -7,15 +7,18 @@ it("should transform a numeric string", () => {
   const result = transformToNumber(input);
 
   expect(result).toBeTypeOf("number");
+  expect(result).toBe(+input);
 });
 
 it("should return NaN for a non-numeric string", () => {
-  const input = "foo";
-  const expectedResult = NaN;
+  const input1 = "foo";
+  const input2 = {};
 
-  const result = transformToNumber(input);
+  const result1 = transformToNumber(input1);
+  const result2 = transformToNumber(input2);
 
-  expect(result).toBe(expectedResult);
+  expect(result1).toBeNaN();
+  expect(result2).toBeNaN();
 });
 
 it("should return NaN if given no argument", () => {
