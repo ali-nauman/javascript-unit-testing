144cb88a09705eac58c6fc8c3a8cf2ac531ecffd
Ali Nauman <alinauman97@outlook.com>
2023-07-21 00:51:01 +0500

    Add tests and fix errors

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -9,3 +9,20 @@ it("should sum all number values in an array", () => {
 
   expect(sum).toBe(expectedSum);
 });
+
+it("should yield NaN if at least one invalid number is provided", () => {
+  const inputs = ["invalid", 1];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string values is provided", () => {
+  const numbers = ["1", "2"];
+  const expectedSum = numbers.reduce((sum, n) => sum + +n, 0);
+
+  const sum = add(numbers);
+
+  expect(sum).toBe(expectedSum);
+});
