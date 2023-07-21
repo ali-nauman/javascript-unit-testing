2220d3fccaabc9e3f780cbe1fa561df5eda271c4
Ali Nauman <alinauman97@outlook.com>
2023-07-21 20:00:37 +0500

    Add tests for generateResultText

---
+++ basic-testing-frontend/src/output.test.js
@@ -0,0 +1,42 @@
+import { describe, expect, it } from "vitest";
+import { generateResultText } from "./output";
+
+describe("generateResultText", () => {
+  it("should return a string no matter what value is passed", () => {
+    const value1 = 1;
+    const value2 = "invalid";
+    const value3 = false;
+
+    const result1 = generateResultText(value1);
+    const result2 = generateResultText(value2);
+    const result3 = generateResultText(value3);
+
+    expect(result1).toBeTypeOf("string");
+    expect(result2).toBeTypeOf("string");
+    expect(result3).toBeTypeOf("string");
+  });
+
+  it("should return a string that contains the calculation result if a number is provided as a result", () => {
+    const result = 5;
+
+    const resultText = generateResultText(result);
+
+    expect(resultText).toContain(result.toString());
+  });
+
+  it('should return an empty string if "no-calc" is provided as a result', () => {
+    const result = "no-calc";
+
+    const resultText = generateResultText(result);
+
+    expect(resultText).toBe("");
+  });
+
+  it('should return a string that contains "Invalid" if "invalid" is provided as a result', () => {
+    const result = "invalid";
+
+    const resultText = generateResultText(result);
+
+    expect(resultText).toContain("Invalid");
+  });
+});
